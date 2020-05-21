2cb8bd9f77b129814776cc84fa29da24036b58e3
younggyu <asd11215@naver.com>
2020-05-21 18:04:32 +0900

    200521

--- front/my-app/src/components/common/NewsNav.vue
+++ front/my-app/src/components/common/NewsNav.vue
@@ -52,7 +52,7 @@
                                         <v-col  cols='12' sm="6"> 
                                         <!-- 카카오 -->
                                         <KakaoLogin
-                                        api-key="8ed60517a6ddc7531af7f7fd4740b174"
+                                        api-key="d067985a2434ec54d14813168409e137"
                                         image="kakao_account_login_btn_medium_narrow_ov"
                                         :on-success=onSuccess
                                         :on-failure=onFailure
@@ -82,47 +82,21 @@
                     </div>
                 </v-card>
             </v-dialog>
-            <!-- <v-dialog v-model="dialog" max-width="520">
-                <v-card>
-                    <v-toolbar flat="flat">
-                        <v-card-title class="headline">News Login</v-card-title>
-                        <v-spacer></v-spacer>
-                        <v-btn icon="icon" dark="dark" @click="loginClear()">
-                            <v-icon color="black darken-2">mdi-close</v-icon>
-                        </v-btn>
-                    </v-toolbar>
-                    <div class="pa-3" style="max-width: 540px">
-                        <v-card-text>
-                            <v-spacer></v-spacer>
-                            <v-row>
-                                <KakaoLogin
-                                    api-key="8ed60517a6ddc7531af7f7fd4740b174"
-                                    image="kakao_account_login_btn_medium_narrow_ov"
-                                    :on-success="onSuccess"
-                                    :on-failure="onFailure"/>
-                            </v-row>
-                            <v-btn
-                                class="mb-1"
-                                depressed="depressed"
-                                block="block"
-                                large="large"
-                                @click="loginClear">취소</v-btn>
-                            <v-sheet v-if="user.login_fail" color="orange lighten-2">존재하지 않는 아이디이거나 비밀번호가 틀립니다.</v-sheet>
-                        </v-card-text>
-                    </div>
-                </v-card>
-            </v-dialog> -->
             <v-divider vertical="vertical"></v-divider>
         </v-toolbar-items>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </v-toolbar>
 </template>
 <script>
-    // import KakaoLogin from 'vue-kakao-login' 
+    import KakaoLogin from 'vue-kakao-login' 
+    import axios from "axios";
+    import { API_BASE_URL } from '../../config'
+    import {login} from "../../api/user.js";
+    import EventBus from "../../EventBus"
     // import { mapState } from "vuex";
     export default {
         components: {
-            // KakaoLogin
+            KakaoLogin
         },
         data() {
             return {
@@ -139,6 +113,54 @@
             // ...mapState([ "isLogin", "isLoginError", "isDialog"])
         },
         methods: {
+            onSuccess(data) {
+                const scope = this;
+                console.log(data)
+                console.log("success")
+                console.log(data.access_token) 
+                localStorage.setItem("access_token1", data.access_token)
+                // data.token은 카카오에서 받은것. 
+                axios
+                .get(API_BASE_URL+"/api/kakao/login", {
+                    headers: {
+                        "jwt-auth-token":data.access_token
+                    }
+                })
+                 .then(response => {
+                    console.log("정원",response);
+                    alert("카카오 로그인에 성공하셨습니다.")
+                    localStorage.setItem("access_token", response.data)
+                     scope
+                        .$store
+                        .dispatch("getMemberInfo")
+                 }); 
+                scope.dialog.login = false;
+            },
+            onFailure(data) {
+                console.log(data)
+                console.log("failure")
+            },
+
+            login: function () {
+                const scope = this;
+                login(scope.user.email, scope.user.password, function (response) {
+                    let token = response.data.auth_token;
+
+                    localStorage.setItem("access_token", token)
+                    scope
+                        .$store
+                        .dispatch("getMemberInfo")
+
+                    EventBus.$emit("login-success");
+
+                    scope.dialog.login = false;
+                }, function (error) {
+                    // alert("유저 이메일 혹은 비밀번호가 일치하지 않습니다.");
+                    scope.user.login_fail = true;
+                    console.error(error);
+                });
+            },
+
             loginClear() {
                 this.dialog = false;
             }, 
