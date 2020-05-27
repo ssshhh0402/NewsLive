e34fa23a2512ccb6ac85974becbee90a761fb1ba
wooou <dksdnwls@gmail.com>
2020-05-27 17:01:40 +0900

    :new: 팔로우 기능 추가

--- back/src/main/java/com/block/chain/news/service/FollowService.java
+++ back/src/main/java/com/block/chain/news/service/FollowService.java
@@ -1,6 +1,10 @@
 package com.block.chain.news.service;
 
+import com.block.chain.news.domain.follow.Follow;
 import com.block.chain.news.domain.follow.FollowRepository;
+import com.block.chain.news.domain.user.User;
+import com.block.chain.news.domain.user.UserRepository;
+import com.block.chain.news.web.dto.user.FollowRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +12,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowService {
     private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+
+    public String follow(FollowRequestDto requestDto){
+        User fromUser = userRepository.findByEmail(requestDto.getFromUserEmail())
+                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다. email =" + requestDto.getFromUserEmail()));
+
+        User toUser = userRepository.findByEmail(requestDto.getToUserEmail())
+                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 없습니다. email =" + requestDto.getFromUserEmail()));
+
+        followRepository.save(Follow.builder()
+            .fromUser(fromUser)
+            .toUser(toUser)
+            .build());
+
+        return toUser.getEmail();
+    }
+
 }
--- back/src/main/java/com/block/chain/news/web/dto/user/FollowRequestDto.java
+++ back/src/main/java/com/block/chain/news/web/dto/user/FollowRequestDto.java
@@ -1,6 +1,15 @@
 package com.block.chain.news.web.dto.user;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class FollowRequestDto {
     // 나의 이이디
     // 팔로우 대상의 아이디
+
+    private String fromUserEmail;
+    private String toUserEmail;
+
 }
