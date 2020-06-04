22a101c2c899a217438a3f8b4ce1767063f16291
ssshhh0402 <ssshhh0402@naver.com>
2020-06-04 11:09:14 +0900

    :hammer: 백엔드 통합중

--- back/src/main/java/com/block/chain/news/config/HttpConnectionConfig.java
+++ back/src/main/java/com/block/chain/news/config/HttpConnectionConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+
 @Configuration
 public class HttpConnectionConfig {
 
