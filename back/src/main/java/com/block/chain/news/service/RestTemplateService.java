7b795636ef011ff29ba391d5b0a2fd9cb773ef05
wooou <dksdnwls@gmail.com>
2020-06-04 22:13:05 +0900

    :new: 번역시 번역된 문장만 반환하도록 추가

--- back/src/main/java/com/block/chain/news/service/RestTemplateService.java
+++ back/src/main/java/com/block/chain/news/service/RestTemplateService.java
@@ -1,9 +1,7 @@
 package com.block.chain.news.service;
 
-import com.block.chain.news.domain.RestTemplateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
