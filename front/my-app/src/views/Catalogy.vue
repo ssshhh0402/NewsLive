1dac58dc2bb861eb428884e4bd1ac88bc136167c
kimjungwon <koo_m@naver.com>
2020-06-07 00:02:02 +0900

    :hammer:

--- front/my-app/src/views/Catalogy.vue
+++ front/my-app/src/views/Catalogy.vue
@@ -1,38 +1,66 @@
 
 <template>
-	<div style="height:400px">
+	<div>
         <br>
-        <v-carousel height =650 width =1000 v-if="allNews.length"  hide-delimiters show-arrows-on-hover> 
-        <v-carousel-item v-for="k in allNews.length/2" :key="k"> 
         <v-hover v-slot:default="{ hover }">
             <v-container>
                 <v-row>
+                    <v-col cols="12"
+                     v-for="(list,index) in allNews"
+                    :key="index"
+                    >
                 <v-card
-                    height="300"
-                    width="400"
+                    style="margin-bottom:10px"
+                    height="20vh"
+                    width="100%"
                     light="light"
                     align-center="align-center"
                     class="mx-auto"
                     :elevation="hover ? 16 : 2"
-                    v-for="(list,index) in allNews"
-                    :key="index">
+                    v-if="(page-1)*5<=index && index<(page-1)*5 +5"
+                    >
                     <div class="div-bc">
+                        <v-row>
+                            <v-col cols="4">
                         <v-img
                             class="white--text align-end"
-                            height="200px"
+                            height="15vh"
+                            width="100%"
                             v-bind:src="list.banner==='NO'?require('../assets/newsBK2.png'):list.banner"
                             >
                             <v-card-title>
-                            <v-btn rounded color="black" dark large>{{list.title}}</v-btn>
+                            
                             </v-card-title>
                         </v-img>
+                        </v-col>
+                        <v-col cols="8">
+                        <span
+                            class=" test font-weight-bold "
+                            style="font-size:25px;">{{list.title}}
+                        </span>
+                        <v-card style="margin-top:1vw; width:88%;">
+                <v-card-text>
+            <h3>keywords</h3>
+            <h4>{{list.topics.length>=50?list.topics.slice(0,50)+"...":list.topics}}</h4>
+                </v-card-text>
+            </v-card>
+                        </v-col>
+                        </v-row>
                     </div>
                 </v-card>
+                </v-col>
             </v-row>
+            
+                 <div class="text-center">
+                    <v-pagination
+                    v-model="page"
+                    :length="Math.floor(allNews.length/5)+1"
+                    prev-icon="mdi-menu-left"
+                    next-icon="mdi-menu-right"
+                    ></v-pagination>
+                </div>       
             </v-container>
         </v-hover>
-        </v-carousel-item>
-        </v-carousel>
 	</div>
 </template>
 <script>
@@ -42,8 +70,20 @@ export default {
   },
   created(){
       console.log(this.allNews)
-  }
+      console.log(this.allNews.length/5)
+  },
+  data () {
+      return {
+        page: 1,
+      }
+    },
 };
 </script>
 
-<style></style>
+<style>
+.test {
+  background: linear-gradient(to right, #fbcac9, #8ca6ce);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
---
+++ front/my-app/src/views/SubHome.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+		<div class= "mt-5">
+    <v-row>
+      <!-- font-family = 'Open Sans' -->
+      <v-col cols="12" md="2"></v-col>
+      <v-col cols="12" md="4">
+        <div align="left" class="mt-7">
+		<span
+			class=" test font-weight-bold "
+			style="font-size:35px;">The News Live
+		</span>
+        <br><br>  
+          <div align="left" class="gray font-weight-bold mt-6" style="font-size:19px;">
+          <div class="h1"><strong>최근 기사 <span class="h5"><strong>~~~~~ </strong></span></strong></div>
+          </div> 
+           <br> 
+           <v-divider></v-divider>
+          <v-btn 
+            class=" mt-5"
+            style="background: #7FD4D9"
+            color="deep-sky"
+            dark
+            large
+          > 
+          <div class="mb-2 mr-2 font-weight-bold ">블록체인, 뉴스확인하시겠어요?</div> 
+          <v-avatar></v-avatar>
+        </v-btn>
+        <v-btn 
+            class=" mt-3 pink"
+            color="deep-sky"
+            dark
+            large
+          > 
+          <div class="mb-2">더뉴스라이브를 소개 페이지 가기</div> 
+          <v-avatar></v-avatar>
+          <v-icon class ="mb-2">mdi-click </v-icon>
+        </v-btn>
+         <br>
+          <br>
+         </div>
+      </v-col> 
+      <v-col  cols="12" md="5" >
+        <v-carousel  hide-delimiters show-arrows-on-hover interval="4000"> 
+        <v-carousel-item v-for="k in 4" :key="k"> 
+            <v-row >
+				<v-hover v-slot:default="{ hover }">
+					<v-card
+						light="light"
+						align-center="align-center"
+						:elevation="hover ? 16 : 2"
+						class="mx-auto">
+						<v-img
+							class="white--text align-end"
+							height="500px"
+							src= "../assets/backnews.jpg"
+						>
+						<v-card-text>
+							
+						</v-card-text>
+						<v-card-actions>
+							<v-btn text="text" color="deep-purple accent-4">
+								Read
+							</v-btn>
+							<v-btn text="text" color="deep-purple accent-4">
+								Bookmark
+							</v-btn>
+						</v-card-actions>
+						</v-img>
+					</v-card>
+			</v-hover>
+            </v-row>
+        </v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+    </div>
+	<br>
+	<div class="back_g" style="height:570px">
+		<br>
+	</div>
+	<div style="background: (58, 76, 168); height:570px">
+		
+	</div>
+</div>
+</template>
+    <script >
+		export default {
+			data() {
+				return {search: null};
+			},
+			created() {},
+			methods: {
+				goSearch(content) {
+					console.log(content)
+				}
+			}
+		}
+    </script>
+    <style>
+	.back_g{
+		background: #654ea3;  /* fallback for old browsers */
+		background: -webkit-linear-gradient(to right, #eaafc8, #654ea3);  /* Chrome 10-25, Safari 5.1-6 */
+		background: linear-gradient(to right, #eaafc8, #654ea3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	}
+	.test {
+		background: linear-gradient(to right, #fbcac9, #8ca6ce);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
+	</style>
