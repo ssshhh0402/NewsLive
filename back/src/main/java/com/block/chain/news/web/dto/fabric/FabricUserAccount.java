1d410c6651009018548fb311556bf2a8edf85ad5
wooou <dksdnwls@gmail.com>
2020-06-07 18:44:45 +0900

    :new: 현재 유효한 광고 불러오기 기능 추가

--- back/src/main/java/com/block/chain/news/web/dto/fabric/FabricUserAccount.java
+++ back/src/main/java/com/block/chain/news/web/dto/fabric/FabricUserAccount.java
@@ -1,5 +1,8 @@
 package com.block.chain.news.web.dto.fabric;
 
+import lombok.Getter;
+
+@Getter
 public class FabricUserAccount {
     String userID;
     String amount;
@@ -7,29 +10,5 @@ public class FabricUserAccount {
     public FabricUserAccount(String userID, String amount) {
         this.userID = userID;
         this.amount = amount;
-    }
-
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    @Override
-    public String toString() {
-        return "FabricUserAccount{" +
-                "userID='" + userID + '\'' +
-                ", amount='" + amount + '\'' +
-                '}';
     }
 }
